27834e7a8b395fc07aad782e2be04eca303b584f
TCHAKPELE <k.tchakpele@whitedev.fr>
2021-08-22 22:57:21 +0200

    fix

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
 const express = require("express");
-var createError = require('http-errors');
-var path = require('path');
-var cors = require("cors");
 var bodyParser = require('body-parser');
 
-var corsOptions = {
-    origin: "http://localhost:3000"
-};
-
-app.use(cors(corsOptions));
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }));
-app.use(cors());
 
 // simple route
 app.get("/", (req, res) => {
